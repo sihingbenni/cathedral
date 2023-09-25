740cf453bd834747928e963527b64c2df59f236f
Benjamin Puhani <benjamin.puhani@student.fh-kiel.de>
2023-09-25 15:41:53 +0200

    FIX the build process

--- src/main/java/de/fhkiel/eki/Main.java
+++ src/main/java/de/fhkiel/eki/Main.java
@@ -1,7 +1,10 @@
-package org.example;
+package de.fhkiel.eki;
+
+import de.fhkiel.ki.cathedral.gui.CathedralGUI;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        CathedralGUI.start();
     }
 }
