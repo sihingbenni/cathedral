bf3358bd6e7a8e9e42f1be9eeb3881d928831e3e
Jane <janeluciaschoenfeld@gmail.com>
2023-09-25 15:10:19 +0200

    add libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,23 @@ plugins {
     id("java")
 }
 
-group = "org.example"
+group = "de.student"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    flatDir {
+        dirs 'libs'
+    }
 }
 
 dependencies {
+    implementation name: 'cathedral-game-1.3'
+    implementation "com.discord4j:discord4j-core:3.2.3"
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
 }
 
 tasks.test {
