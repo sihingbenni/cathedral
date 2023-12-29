26debc68a725cd5088e76331ba3832308a56759f
Benjamin Puhani <benjamin.puhani@student.fh-kiel.de>
2023-12-29 17:55:03 +0100

    MOD use switch instead of if

--- src/main/java/de/fhkiel/eki/boffin/calculator/EarlyGameTurnCalculator.java
+++ src/main/java/de/fhkiel/eki/boffin/calculator/EarlyGameTurnCalculator.java
@@ -10,24 +10,34 @@ public class EarlyGameTurnCalculator implements TurnCalculator {
     @Override
     public Set<Placement> calculateTurn(Game game, Set<Placement> possiblePlacements) {
         int turnNumber = game.lastTurn().getTurnNumber();
+        Board board = game.getBoard();
         gameStateManager.switchGameState(GameState.MidGame);
+        
+        switch (turnNumber) {
+            case 0 -> {
+                // return all possible placements, one will be chosen by random
+                return possiblePlacements;
+            }
+            case 1 -> {
+                // get the location of the cathedral
+                Placement cathedral = board.getPlacedBuildings().get(0);
 
-        // if it's the first turn for white,
-        // place the cathedral in the top left corner
-        if (turnNumber == 0) {
-            // place the cathedral random
-            return possiblePlacements;
-        } else if (turnNumber == 1) {
-            // place the infirmary on the opposite side of the cathedral
-            Placement cathedral = game.getBoard().getPlacedBuildings().get(0);
+                // calculate place opposite of cathedral
+                int x = cathedral.x() > 4 ? 3 : 6;
+                int y = cathedral.y() > 4 ? 2 : 7;
 
-            // calculate place opposite of cathedral
-            int x = cathedral.x() > 4 ? 3 : 6;
-            int y = cathedral.y() > 4 ? 2 : 7;
+                // create a new Placement for the infirmary
+                Placement infirmaryPlacement = new Placement(
+                        new Position(x, y), Direction._0, Building.Black_Infirmary
+                );
 
-            return Collections.singleton(new Placement(new Position(x, y), Direction._0, Building.Black_Infirmary));
+                // return the placement
+                return Collections.singleton(infirmaryPlacement);
+            }
+            default -> throw new RuntimeException("EarlyGameTurnCalculator should not be called" +
+                    " after the first two turns.");
         }
 
-        throw new RuntimeException("EarlyGameTurnCalculator should not be called after the first two turns.");
+
     }
 }
